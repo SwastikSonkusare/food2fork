8da6fd5ae0f91d77e38b55c4be293bdf32bac074
Swastik <swastik.sonkusare18@gmail.com>
2021-08-11 12:24:41 +0530

    added: header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header/Header";
 
+
+
 const App = () => {
   return (
     <>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,41 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faBookmark } from "@fortawesome/free-solid-svg-icons";
+
 import logo from "../../assets/logo.09084f39.png";
+
+import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="header">
       <img className="header__image" src={logo} alt="food2fork"></img>
+      <form className="header__form">
+        <input
+          className="header__input"
+          type="text"
+          placeholder="Search for your favourite recipes"
+        ></input>
+
+        <div className="header__button">
+          <FontAwesomeIcon
+            icon={faSearch}
+            size="2x"
+            className="header__search"
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+
+      <div className="header__bookmarks">
+        <FontAwesomeIcon
+          icon={faBookmark}
+          size="2x"
+          className="header__bookmarks-icon"
+        />
+        <h4>Bookmarks</h4>
+      </div>
     </header>
   );
 };
