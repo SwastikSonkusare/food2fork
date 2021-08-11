6b89e48d144f8b757dbd9f841c2dae1c9a8107c4
Swastik <swastik.sonkusare18@gmail.com>
2021-08-11 11:54:29 +0530

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import logo from "../../assets/logo.09084f39.png";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img className="header__image" src={logo} alt="food2fork"></img>
+    </header>
+  );
+};
+
+export default Header;
